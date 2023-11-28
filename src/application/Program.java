9b8760896e79e950c5fea0b2bdb5079433add54a
Helio Junior <93434324+HelioDevv@users.noreply.github.com>
2023-11-27 23:11:39 -0300

    Finish 2.0

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,11 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,8 +28,44 @@ public class Program {
 		File sourceFile = new File(sourceFileStr);
 		String sourceFolderStr = sourceFile.getParent();
 		
-		System.out.println(sourceFolderStr);
-
+		boolean success = new File(sourceFolderStr + "\\out").mkdir();
+		
+		String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))){
+			String itemCsv = br.readLine();
+			
+			while(itemCsv != null) {
+				
+				String[] fields = itemCsv.split(",");
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+				int quantity = Integer.parseInt(fields[2]);
+				
+				list.add(new Product(name, price, quantity));
+				
+				itemCsv = br.readLine();
+			}
+			
+			try(BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))){
+				
+				for(Product item : list) {
+					bw.write(item.getName() + "," + String.format("%.2f", item.total()));
+					bw.newLine();
+				}
+				
+				System.out.println(targetFileStr + "CREATED");
+				
+			}
+			catch(IOException e) {
+				System.out.println("Error writing file" + e.getMessage());
+			}
+		}
+		catch(IOException e) {
+			System.out.println("Error writing file" + e.getMessage());
+		}
+		
+		sc.close();
 	}
 
 }
